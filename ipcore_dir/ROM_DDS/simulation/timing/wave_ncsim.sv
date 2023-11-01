31f4ab4b1519ec372df990fcfc5aafc064f6de89
Liang <53165250+flagchess@users.noreply.github.com>
2023-11-01 18:35:40 +0800

    Add files via upload

---
+++ ipcore_dir/ROM_DDS/simulation/timing/wave_ncsim.sv
@@ -0,0 +1,20 @@
+
+ 
+ 
+ 
+
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+
+      waveform add -signals /ROM_DDS_tb/status
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/CLKA
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/ADDRA
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/DOUTA
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/CLKB
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/ADDRB
+      waveform add -signals /ROM_DDS_tb/ROM_DDS_synth_inst/bmg_port/DOUTB
+console submit -using simulator -wait no "run"
